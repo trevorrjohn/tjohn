a965e274d5c1f92e4e51cbd29cf8267d458b4143
Trevor John, trevorrjohn <pair+trevor@gmail.com>
2012-09-22 13:59:36 -0400

    Add contact info

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,21 +1,72 @@
-$(document).ready(function() {
+var isTwoPanes = function() {
+  console.log($("#info").width() + 100)
+  console.log("Window width: " + $(window).width()/3)
+  return ($("#info").width() + 50 > $(window).width()/3)
+}
+
+var setSizes = function() {
+  var width = $(window).width();
+  var $base = $("#base");
+  var $red = $("#red");
+  var $orange = $("#orange");
+  var $info = $("#info");
+
   $('body').height($(window).height());
+  if(isTwoPanes()) {
+    $base.hide();
+
+    $red.width(width/2);
+    $orange.width(width/2);
+
+    $orange.offset({left: width/2 - 5});
+  } else {
+    $base.show();
+    $base.width(width/3);
+    $red.width(width/3);
+    $orange.width(width/3);
+
+    $red.offset({left: 5})
+    $orange.offset({left: width/3})
+    $base.offset({left: 2/3 * width - 5})
+  }
+}
+
+$(document).ready(function() {
+  var $base = $("#base");
+  var $red = $("#red");
+  var $info = $("#info");
+  var redToggle = true;
 
   $(window).resize(function() {
-    $('body').height($(window).height());
-  });
-  var redToggle = true;
-  $("#red").click(function() {
-    var width = $(window).width()/3 - 40;
+    setSizes();
+    redToggle = true;
+  })
 
-    if(redToggle) {
-      redToggle = false;
-      $(this).animate({"left": "+=" + width + "px"}, "slow");
+  $red.click(function() {
+    var width = $(window).width();
+    var move = $info.width() + 40;
+
+    if(isTwoPanes()) {
+      if(redToggle) {
+        redToggle = false;
+        console.log('2 slider on');
+        $(".slider").animate({"left": "+=" + move + "px"}, "slow");
+      } else {
+        redToggle = true;
+        console.log('2 slider off');
+        $(".slider").animate({"left": "-=" + move + "px"}, "slow");
+      }
     } else {
-      redToggle = true;
-      $(this).animate({"left": "-=" + width + "px"}, "slow");
+      if(redToggle) {
+        redToggle = false;
+        console.log('3 slider on');
+        $(this).animate({"left": "+=" + move + "px"}, "slow");
+      } else {
+        redToggle = true;
+        console.log('3 slider off');
+        $(this).animate({"left": "-=" + move + "px"}, "slow");
+      }
     }
   })
-});
-
-
+  $(window).trigger('resize');
+})
