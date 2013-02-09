a23176e3c12e9ce344796ca21aacccab3ed0f924
Trevor John <trevor@john.tj>
2013-02-08 22:44:21 -0500

    Refactor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,5 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery_ujs
+//= require raphael
 //= require_tree .
--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,72 +1,13 @@
-var isTwoPanes = function() {
-  console.log($("#info").width() + 100)
-  console.log("Window width: " + $(window).width()/3)
-  return ($("#info").width() + 50 > $(window).width()/3)
-}
-
-var setSizes = function() {
+$(function() {
   var width = $(window).width();
-  var $base = $("#base");
-  var $red = $("#red");
-  var $orange = $("#orange");
-  var $info = $("#info");
+  var height = $(window).height();
+  var $container = $("#container");
 
-  $('body').height($(window).height());
-  if(isTwoPanes()) {
-    $base.hide();
+  $container.width(width);
+  $container.height(height);
 
-    $red.width(width/2);
-    $orange.width(width/2);
-
-    $orange.offset({left: width/2 - 5});
-  } else {
-    $base.show();
-    $base.width(width/3);
-    $red.width(width/3);
-    $orange.width(width/3);
-
-    $red.offset({left: 5})
-    $orange.offset({left: width/3})
-    $base.offset({left: 2/3 * width - 5})
-  }
-}
-
-$(document).ready(function() {
-  var $base = $("#base");
-  var $red = $("#red");
-  var $info = $("#info");
-  var redToggle = true;
-
-  $(window).resize(function() {
-    setSizes();
-    redToggle = true;
-  })
-
-  $red.click(function() {
-    var width = $(window).width();
-    var move = $info.width() + 40;
-
-    if(isTwoPanes()) {
-      if(redToggle) {
-        redToggle = false;
-        console.log('2 slider on');
-        $(".slider").animate({"left": "+=" + move + "px"}, "slow");
-      } else {
-        redToggle = true;
-        console.log('2 slider off');
-        $(".slider").animate({"left": "-=" + move + "px"}, "slow");
-      }
-    } else {
-      if(redToggle) {
-        redToggle = false;
-        console.log('3 slider on');
-        $(this).animate({"left": "+=" + move + "px"}, "slow");
-      } else {
-        redToggle = true;
-        console.log('3 slider off');
-        $(this).animate({"left": "-=" + move + "px"}, "slow");
-      }
-    }
-  })
-  $(window).trigger('resize');
+  var paper = Raphael(document.getElementById("container"), width, height);
+  var circle = paper.circle(width/2, height/2, (width > height ? height/2 : width/2) );
+  circle.attr("fill", "#f00");
+  circle.attr("stroke", "#e2e2e2");
 })
