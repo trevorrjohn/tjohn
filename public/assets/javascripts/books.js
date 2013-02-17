93f854e43fd5b2f85bb01f4d9d684fa7ac29e76e
Trevor John <trevor@john.tj>
2013-02-16 20:08:04 -0500

    JS fix

--- public/assets/javascripts/books.js
+++ public/assets/javascripts/books.js
@@ -1,6 +1,7 @@
-$(".book").load(function() {
-  alert("here");
-  var $container = $(this).parents(".container");
-  $container.width($(this).width())
-  $container.height($(this).height())
-});
+$(function(){
+  $(".book").load(function() {
+    var $container = $(this).parents(".container");
+    $container.width($(this).width())
+    $container.height($(this).height())
+  });
+})
