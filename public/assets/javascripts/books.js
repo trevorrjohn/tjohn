351df629661d5be184ff90d2820d2909717b80fb
Trevor John <trevor@john.tj>
2013-02-16 20:51:36 -0500

    Working with masonry

--- public/assets/javascripts/books.js
+++ public/assets/javascripts/books.js
@@ -4,4 +4,13 @@ $(function(){
     $container.width($(this).width())
     $container.height($(this).height())
   });
-})
+
+  var $wrapper = $("#wrapper");
+  $wrapper.imagesLoaded(function(){
+    $wrapper.masonry({
+      itemSelector : '.container',
+      columnWidth : 240,
+      isAnimated : true
+    });
+  })
+});
