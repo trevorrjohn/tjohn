ff1ea9a36f13108b64c5816d71a00a2b927b85e9
Trevor John <trevor@john.tj>
2013-02-18 19:46:26 -0500

    Hide overflow in Firefox

--- public/assets/javascripts/books.js
+++ public/assets/javascripts/books.js
@@ -1,15 +1,22 @@
 $(function(){
-  $(".book").load(function() {
-    var $container = $(this).parents(".container");
-    $container.width($(this).width())
-    $container.height($(this).height())
+  $("img.book").load(function() {
+    var $this = $(this);
+    var width = $this.width();
+    var height = $this.height();
+    var scale = 1;
+
+    if($this.parent(".face").next(".face").children("p").text() == "") {
+      scale = 0.75;
+      $this.css({ height: height * scale, width: width * scale });
+    }
+
+    $this.parents(".container").css({ height: height * scale, width: width * scale });
   });
 
   var $wrapper = $("#wrapper");
   $wrapper.imagesLoaded(function(){
     $wrapper.masonry({
       itemSelector : '.container',
-      columnWidth : 240,
       isAnimated : true
     });
   })
