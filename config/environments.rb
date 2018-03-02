834b99e8c0df9a42790bf95f90ac284c4af275e0
Trevor John <trevor@john.tj>
2018-03-01 23:47:06 -0500

    Use puma
    - add schema.rb
    - fix migration after upgrade

--- config/environments.rb
+++ config/environments.rb
@@ -1,6 +1,6 @@
 configure :production, :development do
   # Database connection
-  db = URI.parse(ENV['DATABASE_URL'] || ENV['HEROKU_POSTGRESQL_BRONZE_URL'] || 'postgres://localhost/tjohn_development')
+  db = URI.parse(ENV['DATABASE_URL'] || ENV['HEROKU_POSTGRESQL_BRONZE_URL'] || 'postgres://localhost/tjohn_dev')
 
   ActiveRecord::Base.establish_connection(
     adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
--- db/migrate/20130216052028_create_books_table.rb
+++ db/migrate/20130216052028_create_books_table.rb
@@ -1,4 +1,4 @@
-class CreateBooksTable < ActiveRecord::Migration
+class CreateBooksTable < ActiveRecord::Migration[4.2]
   def up
     create_table :books do |t|
       t.text :title
